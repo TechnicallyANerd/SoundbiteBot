6f7cf5565acdb64621abf71558e76aae7a88171d
TechnicallyANerd <technically.a.nerd@gmail.com>
2018-01-01 17:24:04 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,13 +8,13 @@ client.on("ready", () => {
 client.on("message", (message) => {
   if(message.author.bot) return;
   if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+    message.channel.send("`pong!`");
   }
   if (message.content.startsWith("pong")) {
-    message.channel.send("fuck off xo");
+    message.channel.send("`fuck off xo`");
   }
   if (message.content.startsWith("Ping")) {
-    message.channel.send("nope");
+    message.channel.send("`nope`");
   }
 });
 
