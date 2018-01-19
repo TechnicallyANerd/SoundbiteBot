d6c396f3aea88b688fff7385808177ceb8b74480
IS0metric <swgbhero@ymail.com>
2018-01-19 20:40:05 +0000

    Attempting to fix port binding issue

--- bot2.js
+++ bot2.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const request = require ("request");
 const http = require('http');
 
 client.on("ready", () => {
@@ -63,3 +62,16 @@ client.on("message", (message) => {
 });
 
 client.login(process.env.BOT_TOKEN);
+
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
