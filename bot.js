e04fe5e94b00dba5055f96da2370f2059c73a080
IS0metric <swgbhero@ymail.com>
2018-02-03 22:31:51 +0000

    Tidied up weird bot stuff

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var request = require ("request");
 const http = require('http');
 
 client.on("ready", () => {
@@ -22,8 +21,47 @@ client.on("message", (message) => {
     console.log("works");
     message.channel.send("`works`");
   }
-  if (message.content.startsWith("soundboardtopten")) {
-    message.channel.send("`running...`");
+  if (message.content.startsWith("soundboard_topplayed")) {
+    message.channel.send("Top 4 most played sounds:");
+    var resp_string = 'none';
+    var http = require('http');
+    http.get('http://www.oxsoundboard.com/api/get_top/', (res) => {
+      const { statusCode } = res;
+      const contentType = res.headers['content-type'];
+
+      let error;
+      if (statusCode !== 200) {
+        error = new Error('Request Failed.\n' +
+                          `Status Code: ${statusCode}`);
+      } else if (!/^application\/json/.test(contentType)) {
+        error = new Error('Invalid content-type.\n' +
+                          `Expected application/json but received ${contentType}`);
+      }
+      if (error) {
+        console.error(error.message);
+        // consume response data to free up memory
+        res.resume();
+        return;
+      }
+
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', (chunk) => { rawData += chunk; });
+      res.on('end', () => {
+        try {
+          const parsedData = JSON.parse(rawData);
+          var result = parsedData.data;
+          for (var i = 0; i < result.length; i++){
+            message.channel.send(result[i]);
+          }
+        } catch (e) {
+          console.error(e.message);
+        }
+      });
+    }).on('error', (e) => {
+      console.error(`Got error: ${e.message}`);
+    });
+
   }
 });
 
