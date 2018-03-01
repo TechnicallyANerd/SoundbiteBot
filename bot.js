01d9be10240a0b9230b49593cca3585d400c6add
IS0metric <swgbhero@ymail.com>
2018-03-01 13:19:37 +0000

    Added link fn

--- bot.js
+++ bot.js
@@ -1,62 +1,65 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const http = require('http');
+const prexif = "!sb";
+const path = "http://oxsoundboard.com/";
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-  if(message.author.bot) return;
-  if (message.content.startsWith("ping")) {
-    message.channel.send("`pong!`");
-  }
-  if (message.content.startsWith("pong")) {
-    message.channel.send("`fuck off xo`");
-  }
-  if (message.content.startsWith("Ping")) {
-    message.channel.send("`nope`");
-  }
-  if (message.content.startsWith("soundboard_topplayed")) {
-    message.channel.send("Top 4 most played sounds:");
-    var resp_string = 'none';
-    var http = require('http');
-    http.get('http://www.oxsoundboard.com/api/get_top/', (res) => {
-      const { statusCode } = res;
-      const contentType = res.headers['content-type'];
+  if (message.author.bot) return;
+  if (message.content.startsWith(prefix)) {
+    var split_message = massage.split(" ");
+    if (split_message.length() > 1){
+      if (split_message[1] == "top"){
+        message.channel.send("Top 4 most played sounds:");
+        var resp_string = 'none';
+        var http = require('http');
+        http.get('http://www.oxsoundboard.com/api/get_top/', (res) => {
+          const { statusCode } = res;
+          const contentType = res.headers['content-type'];
 
-      let error;
-      if (statusCode !== 200) {
-        error = new Error('Request Failed.\n' +
-                          `Status Code: ${statusCode}`);
-      } else if (!/^application\/json/.test(contentType)) {
-        error = new Error('Invalid content-type.\n' +
-                          `Expected application/json but received ${contentType}`);
-      }
-      if (error) {
-        console.error(error.message);
-        // consume response data to free up memory
-        res.resume();
-        return;
-      }
-
-      res.setEncoding('utf8');
-      let rawData = '';
-      res.on('data', (chunk) => { rawData += chunk; });
-      res.on('end', () => {
-        try {
-          const parsedData = JSON.parse(rawData);
-          var result = parsedData.data;
-          for (var i = 0; i < result.length; i++){
-            message.channel.send(result[i]);
+          let error;
+          if (statusCode !== 200) {
+            error = new Error('Request Failed.\n' +
+                              `Status Code: ${statusCode}`);
+          } else if (!/^application\/json/.test(contentType)) {
+            error = new Error('Invalid content-type.\n' +
+                              `Expected application/json but received ${contentType}`);
           }
-        } catch (e) {
-          console.error(e.message);
-        }
-      });
-    }).on('error', (e) => {
-      console.error(`Got error: ${e.message}`);
-    });
+          if (error) {
+            console.error(error.message);
+            // consume response data to free up memory
+            res.resume();
+            return;
+          }
+
+          res.setEncoding('utf8');
+          let rawData = '';
+          res.on('data', (chunk) => { rawData += chunk; });
+          res.on('end', () => {
+            try {
+              const parsedData = JSON.parse(rawData);
+              var result = parsedData.data;
+              for (var i = 0; i < result.length; i++){
+                message.channel.send(result[i]);
+              }
+            } catch (e) {
+              console.error(e.message);
+            }
+          });
+        }).on('error', (e) => {
+          console.error(`Got error: ${e.message}`);
+        });
+      }
+      if (split_message[1] == "link" && split_message.length() == 3){
+        var link = "`" + path + split_message[2] + "`";
+        message.channel.send(link);
+      }
+    }
+
 
   }
 });
